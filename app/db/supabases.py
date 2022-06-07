b3417acbf3ab08ecdda42d8c1adffaf7c857d9c3
MathieuD-QA <mathieu.dassie@protonmail.com>
2022-06-07 22:14:05 +0200

    second commit

--- app/db/supabases.py
+++ app/db/supabases.py
@@ -1,6 +1,7 @@
 from supabase import create_client, Client
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 url = os.environ.get("SUPABASE_URL")
@@ -13,22 +14,17 @@ def get_show():
     show = supabase.table('Ramen-Reviews').select('*').execute()
     return show
 
-def get_index(payload = ""):
-    pass
+
+def get_index(payload):
+    index = supabase.table('Ramen-Reviews').select('*').eq('Review', payload).execute()
+    return index
 
 
-def post_created(payload = ""):
-    pass
-
-def delete_index(payload = ""):
-    pass
+def post_created(payload):
+    created = supabase.table('Ramen-Reviews').insert([payload]).execute()
+    return created
 
 
-
-
-
-
-
-
-
-
+def delete_index(payload):
+    deleted = supabase.table('Ramen-Reviews').delete().eq('Review', payload).execute()
+    return deleted
